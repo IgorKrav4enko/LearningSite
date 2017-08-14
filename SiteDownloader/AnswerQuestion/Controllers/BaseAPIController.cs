414148a6654a1c25bd4a7b27af130614402ccaa6
igor <ikrav4enko@gmai.com>
2017-08-14 19:06:20 +0300

    add web api controller

---
+++ SiteDownloader/AnswerQuestion/Controllers/BaseAPIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using AnswerQuestion.DAL;
+using Newtonsoft.Json;
+
+namespace AnswerQuestion.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly AnsweQuestionContext DB = new AnsweQuestionContext();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ SiteDownloader/AnswerQuestion/Controllers/QAAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnswerQuestion.Models;
+
+namespace AnswerQuestion.Controllers
+{
+    public class QAAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(DB.QAs.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]QA value)
+        {
+            DB.QAs.Add(value);
+            return ToJson(DB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]QA value)
+        {
+            DB.Entry(value).State = EntityState.Modified;
+            return ToJson(DB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            DB.QAs.Remove(DB.QAs.FirstOrDefault(x => x.Id == id));
+            return ToJson(DB.SaveChanges());
+        }
+    }
+}
